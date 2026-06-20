2204b89bbacdc4962fd0e3625e1dd90b5861c859
Srinidhi <bhonagirisrinidhi@gmail.com>
2026-06-20 07:03:21 +0530

    Added Toggle Feature

--- frontend/js/indexl.js
+++ frontend/js/indexl.js
@@ -1,2 +1,22 @@
-//alert("Hello from js");
-console.log("Hello from js");
+console.log("JS Loaded");
+
+let isMale = true;
+
+const btn = document.getElementById("togglebtn");
+const img = document.getElementById("userimg");
+const name = document.getElementById("username");
+const gender = document.getElementById("usergender");
+
+btn.addEventListener("click", function () {
+    if (isMale) {
+        img.src = "../images/Jane - Copy.png";
+        name.innerText = "Jane Doe";
+        gender.innerText = "Female";
+    } else {
+        img.src = "../images/john.png";
+        name.innerText = "John Doe";
+        gender.innerText = "Male";
+    }
+
+    isMale = !isMale;
+});
